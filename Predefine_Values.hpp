e28f19dc06f6cba5a08e45a517c1aa4441faf27d
roksanaShimu <rshimu@gmail.com>
2016-08-20 21:14:45 -0600

    Create Predefine_Values.hpp

---
+++ Predefine_Values.hpp
@@ -0,0 +1,13 @@
+#ifndef __PREDEFINE_VALUES_HPP
+#define __PREDEFINE_VALUES_HPP
+
+
+#define length_of_from_v 21 //21
+
+#define warp_size 32
+
+#define number_of_blocks 64 //128//64 //32
+#define threads_per_block 128
+
+
+#endif
